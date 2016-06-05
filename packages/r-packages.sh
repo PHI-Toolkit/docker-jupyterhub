7088c36c1f6040bb27fffae0cb313ce4ff638eab
Herman Tolentino <herman.tolentino@gmail.com>
2016-06-05 10:12:13 -0400

    corrected errors in R package build

--- packages/r-packages.sh
+++ packages/r-packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt-get update
 apt-get install -y libmysqlclient* unixodbc unixodbc-dev \
-  libxml2-dev libglu1-mesa-dev libgsl0ldbl libgsl0-dev libx11-dev mesa-common-dev libglu1-mesa-dev
+  libxml2-dev libglu1-mesa-dev libgsl0ldbl libgsl0-dev libx11-dev mesa-common-dev libglu1-mesa-dev libssh2-1-dev \
+  libcairo2-dev libxt-dev
 Rscript /home/$NB_USER/packages/essential-packages.R
