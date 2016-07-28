7843848339beabe98e5adfb1e215d54f475af2bb
Herman Tolentino <herman.tolentino@gmail.com>
2016-07-27 22:22:26 -0400

    database update

--- packages/python-databases.sh
+++ packages/python-databases.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+
 source /venv27/bin/activate
-pip2 install py2neo
+pip2 install cycli py2neo neo4j-driver neo4jrestclient
 pip2 install psycopg2
 pip2 install mysqlclient
 pip3 install redis
 deactivate
 
 source /venv35/bin/activate
-pip3 install py2neo
+pip3 install cycli py2neo neo4j-driver neo4jrestclient
 pip3 install psycopg2
 pip3 install mysqlclient
 pip3 install redis
